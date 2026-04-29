59a547030b311e7869437f26d5e359970d895fcf
Thien-2008 <doanthanhthien23042008@gmail.com>
2026-04-30 01:08:06 +0700

    Fix callback route to use @supabase/ssr

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,16 +1,31 @@
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
+import { createServerClient } from '@supabase/ssr'
 
 export async function GET(request: Request) {
   const requestUrl = new URL(request.url)
   const code = requestUrl.searchParams.get('code')
 
   if (code) {
-    const supabase = createRouteHandlerClient({ cookies })
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          get(name) {
+            return cookies().get(name)?.value
+          },
+          set(name, value, options) {
+            cookies().set(name, value, options)
+          },
+          remove(name, options) {
+            cookies().set(name, '', { ...options, maxAge: 0 })
+          },
+        },
+      }
+    )
     await supabase.auth.exchangeCodeForSession(code)
   }
 
-  // Chuyển về trang đăng nhập với thông báo xác nhận thành công
   return NextResponse.redirect(new URL('/auth?mode=login&confirmed=true', request.url))
 }
